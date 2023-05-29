3fd457dec36d95747392d038a35741d9770a2bcd
schuquiej-2018354 <schuquiej-2018354@kinal.edu.gt>
2023-05-29 08:09:15 -0600

    Models Product, Services, TypeAccount, User

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -1,1 +1,40 @@
 'use strict'
+
+const mongoose = require('mongoose')
+
+const userSchemma = mongoose.Schema({
+    name: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    noAccount: {
+        type: Number
+    },
+    DPI: {
+        type: Number
+    },
+    adress: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    work: {
+        type: String
+    },
+    salary: {
+        type: Number
+    }
+}, {
+    versionKey: false
+})
+
+module.exports = mongoose.model('User', userSchemma);
